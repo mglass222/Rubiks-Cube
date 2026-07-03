7f2cc4bb854163cad5724da5e3a3bf079660fdbb
mglass222 <matt.glass@gmail.com>
2026-07-03 15:22:15 -0400

    Fix cube rotation sticker colors

--- js/renderer.js
+++ js/renderer.js
@@ -8,6 +8,7 @@ import {
   faceAxis,
   getLayerCubies,
   moveToAngle,
+  rotateCoords,
 } from "./cube.js";
 
 const GAP = 0.058;
@@ -576,34 +577,5 @@ export class CubeRenderer {
     const engineDir = -faceAxis(face).dir;
     const prime = dragDir !== engineDir;
     return prime ? `${face}'` : face;
-  }
-}
-
-function rotateCoords(x, y, z, face, turns) {
-  if (turns === 2) {
-    switch (face) {
-      case "U":
-      case "D":
-        return [-x, y, -z];
-      case "R":
-      case "L":
-        return [x, -y, -z];
-      case "F":
-      case "B":
-        return [-x, -y, z];
-      default:
-        return [x, y, z];
-    }
-  }
-
-  const s = turns === 3 ? -1 : 1;
-  switch (face) {
-    case "U": return [s * z, y, -s * x];
-    case "D": return [-s * z, y, s * x];
-    case "R": return [x, -s * z, s * y];
-    case "L": return [x, s * z, -s * y];
-    case "F": return [s * y, -s * x, z];
-    case "B": return [-s * y, s * x, z];
-    default: return [x, y, z];
   }
 }
